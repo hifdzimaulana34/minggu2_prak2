5b41bb4682a81602a908bf4688d6ff40afb0641a
hifdzimaulana34 <hifdzimaulana.19934@gmail.com>
2021-09-07 22:28:47 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route:: get('/',[PageController::class,'indexLaravel']);
+
+Route:: get('/about',[PageController::class,'AboutUser']);
+
+Route:: get('/article/{id}',[PageController::class,'Article']);
+Route:: get('/',[HomeController::class,'indexLaravel']);
+Route:: get('/about',[AboutController::class,'AboutUser']);
+Route:: get('/article/{id}',[ArticleController::class,'Article']); 
